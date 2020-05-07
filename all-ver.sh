0f58102d117edf09977df8c9d4c935ddf1345cd1
Magnus Glantz <sudo@redhat.com>
2020-05-07 10:53:37 +0200

    Create all-ver.sh

---
+++ all-ver.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Prereqs
+if ! whoami|grep root >/dev/null
+then
+	echo "$0: You need to be root to run this."
+	exit 1
+fi
+
+which rpm >/dev/null
+if [ "$?" -eq 0 ]; then
+	RPM="yes"
+else
+	which apk
+	if [ "$?" -eq 0 ]; then
+		APK="yes"
+	else
+		which apt-get
+		if [ "$?" -eq 0 ]; then
+			APTGET="yes"
+		else
+			echo "$0: Unknown package manager"
+			exit 1
+		fi
+	fi
+fi
+
+if [ "$RPM" == "yes" ]; then
+	dnf install -y file binutils >/dev/null
+elif [ "$APK" == "yes" ]; then
+	apk add file binutils
+elif [ "$APTGET" == "yes" ]; then
+	apt-get update
+	apt-get -y install binutils file wget
+fi
+
+if echo $2|grep -i new >/dev/null
+then
+	  rm -f $1*
+		for file in $(find . -type f|grep -v kernel)
+		do
+				if file $file|grep "ELF 64" >/dev/null
+				then
+					filemd5=$(md5sum $file|awk '{ print $1 }')  
+					for syscall in $(nm --with-symbol-versions $file 2>/dev/null|egrep -iw '(u|w)'|awk '{ print $2 }')
+					do
+						then
+							echo "$filemd5 $syscall"
+						fi
+					done
+					for syscall in $(nm --with-symbol-versions $file 2>/dev/null|egrep -iw '(t|d)' |awk '{ print $3 }')
+					do
+						then
+							echo "$filemd5 $syscall"
+						fi
+					done
+				fi
+		done|tr '[:upper:]' '[:lower:]'|sort -u>$1.all.deps
+	fi
+fi
