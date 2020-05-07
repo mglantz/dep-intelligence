aca7d178a96b9317c2af2cca15e9ea3274eeb060
Magnus Glantz <sudo@redhat.com>
2020-05-07 10:55:39 +0200

    Update all-ver.sh

--- all-ver.sh
+++ all-ver.sh
@@ -37,22 +37,18 @@ fi
 if echo $2|grep -i new >/dev/null
 then
 	  rm -f $1*
-		for file in $(find . -type f|grep -v kernel)
+		for file in $(find . -type f)
 		do
 				if file $file|grep "ELF 64" >/dev/null
 				then
 					filemd5=$(md5sum $file|awk '{ print $1 }')  
 					for syscall in $(nm --with-symbol-versions $file 2>/dev/null|egrep -iw '(u|w)'|awk '{ print $2 }')
 					do
-						then
-							echo "$filemd5 $syscall"
-						fi
+						echo "$filemd5 $syscall"
 					done
 					for syscall in $(nm --with-symbol-versions $file 2>/dev/null|egrep -iw '(t|d)' |awk '{ print $3 }')
 					do
-						then
-							echo "$filemd5 $syscall"
-						fi
+						echo "$filemd5 $syscall"
 					done
 				fi
 		done|tr '[:upper:]' '[:lower:]'|sort -u>$1.all.deps
